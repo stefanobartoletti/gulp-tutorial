c8bc78d62349fc5bd5a305a0c207bee70c3705b7
stefano-b <bartoletti.stefano@gmail.com>
2019-03-12 10:24:07 +0100

    autoprefixer + sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,38 @@
 // gulp requirements
-var gulp = require( 'gulp' );
-var rename = require ( 'gulp-rename' );
-var sass = require ( 'gulp-sass' );
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 // CSS
 
 var styleSRC = './src/sass/style.scss';
 var styleDIST = './dist/css/';
 
-gulp.task( 'style', function() {
+gulp.task('style', function () {
+
     gulp.src(styleSRC)
+
+        .pipe(sourcemaps.init())
+
         .pipe(sass({
             errorLogToConsole: true,
             outputStyle: 'compressed'
         }))
         .on('error', console.error.bind(console))
-        .pipe(rename({suffix:'.min'}))
+
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+
+        .pipe(rename({
+            suffix: '.min'
+        }))
+
+        .pipe(sourcemaps.write('./'))
+
         .pipe(gulp.dest(styleDIST));
-        
+
 });
